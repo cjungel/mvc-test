8838b9a10c5d6f4e5f7fcd4e279bc7c55997396d
Cristian Junge <cristian.junge@nuwit.com>
2013-11-12 10:04:44 -0300

    Simple controller that renders a hello world jsp view.

---
+++ src/main/java/org/cjunge/spring/mvc_test/HomeController.java
@@ -0,0 +1,18 @@
+package org.cjunge.spring.mvc_test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(ModelMap model) {
+
+		return "index";
+
+	}
+
+}
